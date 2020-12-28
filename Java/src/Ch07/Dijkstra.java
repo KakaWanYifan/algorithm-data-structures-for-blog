3a043897421471634aa96aa234f1fe1b7faec48a
KakaWanYifan <registermail@qq.com>
2020-12-29 01:18:50 +0800

    commit

---
+++ Java/src/Ch07/Dijkstra.java
@@ -0,0 +1,15 @@
+package Ch07;
+
+import java.util.HashMap;
+
+public class Dijkstra {
+
+    /**
+     *
+     * @param from from节点
+     * @return from节点到其他节点的距离
+     */
+    public static HashMap<Node,Integer> dijkstra_01(Node from){
+        return null;
+    }
+}
---
+++ Java/src/Ch07/Prim.java
@@ -0,0 +1,45 @@
+package Ch07;
+
+import java.util.*;
+
+public class Prim {
+    /**
+     * 最小生成树的 Prim算法
+     * @param graph 图
+     * @return 最小生成树
+     */
+    public  static HashMap<Node, HashSet<Edge>> prim(Graph graph){
+        HashMap<Node,HashSet<Edge>> rnt = new HashMap<>();
+        // 遍历所有的节点，每个节点都可以作为起始节点
+        for (Node node:graph.nodes.values()){
+            // 已经连通的node
+            HashSet<Node> nodeSet = new HashSet<>();
+            // 已经选择的edge
+            HashSet<Edge> edgeSet = new HashSet<>();
+
+            // 第一个节点被选择
+            nodeSet.add(node);
+            // 可以选择的边，小根堆
+            // 第一个节点的所有的边，都加入可选边啊
+            PriorityQueue<Edge> edgeQueue = new PriorityQueue<>(node.edges);
+            // 只要还右边可选，就进入循环
+            while (!edgeQueue.isEmpty()){
+                // 可选边中的最小边
+                Edge edge = edgeQueue.poll();
+                // toNode 该节点或许能加入
+                Node toNode = edge.to;
+                // 如果没有加入过
+                if (!nodeSet.contains(toNode)){
+                    // 添加进已连通的node
+                    nodeSet.add(toNode);
+                    // 添加进已选择的edge
+                    edgeSet.add(edge);
+                    // 把新的可选边，加入队列
+                    edgeQueue.addAll(toNode.edges);
+                }
+            }
+            rnt.put(node,edgeSet);
+        }
+        return rnt;
+    }
+}
