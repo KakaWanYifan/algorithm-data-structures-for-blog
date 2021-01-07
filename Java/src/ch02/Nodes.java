1e67c71e12f3be6a5266cdd26d42289b42410360
KakaWanYifan <registermail@qq.com>
2021-01-08 03:07:54 +0800

    Ch02

---
+++ Java/src/ch02/Nodes.java
@@ -0,0 +1,46 @@
+package ch02;
+
+/**
+ * 两类节点
+ */
+public class Nodes {
+
+    /**
+     * 单向链表的节点
+     */
+    public class Node{
+        // 节点的值
+        public int value;
+        // 下一个节点
+        public Node next;
+
+        /**
+         * 构造方法
+         * @param data 节点的值
+         */
+        public Node (int data){
+            value = data;
+        }
+    }
+
+    /**
+     * 双向链表的节点
+     */
+    public class DulNode{
+        // 节点的值
+        public int value;
+        // 前一个节点
+        public DulNode prev;
+        // 下一个节点
+        public DulNode next;
+
+        /**
+         *
+         * @param data
+         */
+        public DulNode (int data){
+            value = data;
+        }
+    }
+
+}
--- Java/src/ch02/ReverseLinkedList.java
+++ Java/src/ch02/ReverseLinkedList.java
@@ -1,55 +1,25 @@
 package ch02;
 
-public class ReverseList {
+import ch02.Nodes.Node;
+import ch02.Nodes.DulNode;
 
-    /**
-     * 单向链表的节点
-     */
-    public class Node{
-        // 节点的值
-        public int value;
-        // 下一个节点
-        public Node next;
-
-        /**
-         * 构造方法
-         * @param data 节点的值
-         */
-        public Node (int data){
-            value = data;
-        }
-    }
-
-    /**
-     * 双向链表的节点
-     */
-    public class DulNode{
-        // 节点的值
-        public int value;
-        // 前一个节点
-        public DulNode prev;
-        // 下一个节点
-        public DulNode next;
-
-        /**
-         *
-         * @param data
-         */
-        public DulNode (int data){
-            value = data;
-        }
-    }
+/**
+ *
+ */
+public class ReverseLinkedList {
 
     /**
      * 单向列表的反转
      * @param head 头节点
      * @return 反转之后链表的head
      */
-    public static Node reverseLinkedList(Node curr){
+    public static Node reverseLinkedList(Node head){
         // 前一个节点
         Node prev = null;
         // 下一个节点
         Node next = null;
+        // 当前节点
+        Node curr = head;
         // 如果当前节点不等于空，说明还没反转完
         while (curr != null){
             // 把当前节点的下一个节点，赋值给next
@@ -58,9 +28,9 @@ public class ReverseList {
             // 当前节点的下一个节点赋值为prev，反转
             curr.next = prev;
 
+            // 为下一轮循环做准备
             // 把当前节点作为前一个节点
             // 把下一个节点作为当前节点
-            // 进入下一轮的循环
             prev = curr;
             curr = next;
         }
@@ -73,11 +43,13 @@ public class ReverseList {
      * @param head 头节点
      * @return 反转之后链表的head
      */
-    public static DulNode reverseLinkedList(DulNode curr){
+    public static DulNode reverseLinkedList(DulNode head){
         // 前一个节点
         DulNode prev = null;
         // 下一个节点
         DulNode next = null;
+        // 当前节点
+        DulNode curr = head;
         // 如果当前节点不等于空，说明还没反转完
         while (curr != null){
             // 把当前节点的下一个节点，赋值给next
@@ -89,13 +61,14 @@ public class ReverseList {
             curr.next = prev;
             curr.prev = next;
 
+            // 为下一轮循环做准备
             // 把当前节点作为前一个节点
             // 把下一个节点作为当前节点
-            // 进入下一轮的循环
             prev = curr;
             curr = next;
         }
         // 如果当前节点是空的话，那么前一个节点就一定是反转之后链表的head
         return prev;
     }
+
 }
